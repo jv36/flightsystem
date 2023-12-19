0cb50ebdacdb17338140fe311c1aab9a292dcd84
Clara Sousa <clarasousa2004@hotmail.com>
2023-12-19 10:08:08 +0000

    Funções do Graph.cpp e adição de um printGraph

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,4 +2,36 @@
 // Created by joaovicente on 18-12-2023.
 //
 
+#include <iostream>
 #include "Graph.h"
+
+void Graph::addNode(Airport* airport) {
+    nodes.push_back({ airport, {}, false, 0.0 });
+}
+
+void Graph::addEdge(Airport* source, Airport* destination, Airline* airline, float distance) {
+    for (auto& node : nodes) {
+        if (node.airport == source) {
+            node.adj.push_back({ destination, airline, distance });
+            nodes.push_back({ destination, {}, false, 0.0 });
+            nodes.back().adj.push_back({ source, airline, distance });
+            break;
+        }
+    }
+}
+
+void Graph::setUnvisited() {
+    for (auto& node : nodes) {
+        node.visited = false;
+    }
+}
+
+void Graph::printGraph() {
+    for (const auto& node : nodes) {
+        std::cout << "Airport: " << node.airport->getName() << "\nEdges:\n";
+        for (const auto& edge : node.adj) {
+            std::cout << "  To: " << edge.destAirport->getName() << ", Airline: " << edge.airline->getName() << ", Distance: " << edge.distance << "\n";
+        }
+        std::cout << "\n";
+    }
+}
--- src/Graph.h
+++ src/Graph.h
@@ -11,26 +11,28 @@
 #include "Airport.h"
 
 class Graph {
-
     struct Edge {
-        std::string dest;
-        std::string airport;
+        Airport* destAirport;
+        Airline* airline;
+        float distance;
     };
 
     struct Node {
         Airport* airport;
         std::list<Edge> adj;
         bool visited;
-        int distance;
-        // precisa de mais
+        float distance;
+        //vamos provavelmente precisar de mais
     };
+
+    std::vector<Node> nodes;
 
 public:
     Graph();
-    void addNode();
-    void addEdge();
+    void addNode(Airport* airport);
+    void addEdge(Airport* source, Airport* destination, Airline* airline, float distance);
     void setUnvisited();
-
+    void printGraph(); // Helper function -> talvez mudar para main/menu no futuro
 };
 
 
